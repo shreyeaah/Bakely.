fbe32bfe899d56d6b2d5fb827eace467c344c5bc
shreyeaah <shreyasusanshibu@gmail.com>
2025-06-25 19:25:10 +0530

    admin pass handled securely

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from app import app, db, bcrypt, login_manager
 from models import User, BakerProfile, MenuItem, CartItem, Order, BakerPricing
 from forms import RegisterForm, LoginForm, ApproveForm, DeclineForm, MenuItemForm, OrderForm, BakerProfileForm, BakerPricingForm
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
 import os
 from sqlalchemy import func
 import requests
@@ -32,13 +33,18 @@ def create_admin():
         if existing_admin:
             return "Admin already exists."
 
-        hashed_pw = bcrypt.generate_password_hash("admin123").decode('utf-8')
+        # Get password from env
+        admin_password = os.getenv("ADMIN_PASSWORD")
+        if not admin_password:
+            return "Admin password not set!", 500
+
+        hashed_pw = generate_password_hash(admin_password, method='sha256')
         admin = User(username='admin', password=hashed_pw, role='admin', is_approved=True)
         db.session.add(admin)
         db.session.commit()
         return "Admin user created successfully."
     except Exception as e:
-        return f"Failed to create admin: {str(e)}"
+        return f"Failed to create admin: {str(e)}", 500
 
 
 @app.route('/')
